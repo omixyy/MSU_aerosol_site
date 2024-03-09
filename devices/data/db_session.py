0a2d1d96b6ba97602ecee47130f50972bb4b4663
Ivan Kaziev <ivkaziev@yandex.ru>
2024-03-10 01:09:25 +0300

    Fix flake8

--- devices/data/db_session.py
+++ devices/data/db_session.py
@@ -2,6 +2,11 @@ import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 
+__all__ = [
+    "global_init",
+    "create_session",
+]
+
 SqlAlchemyBase = orm.declarative_base()
 
 __factory = None
@@ -16,13 +21,11 @@ def global_init(db_file):
     if not db_file or not db_file.strip():
         raise Exception("Необходимо указать файл базы данных.")
 
-    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    conn_str = f"sqlite:///{db_file.strip()}?check_same_thread=False"
     print(f"Подключение к базе данных по адресу {conn_str}")
 
     engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
-
-    from . import __all_models
 
     SqlAlchemyBase.metadata.create_all(engine)
 
--- main.py
+++ main.py
@@ -2,28 +2,29 @@ from config import DEBUG, STATIC_URL
 from flask import Flask
 from graph_funcs import make_graph, preprocessing_all_files
 
-from homepage.home import home_bp
-from devices.device import device_bp
 from devices.data import db_session
+from devices.device import device_bp
+
+from homepage.home import home_bp
+
+__all__ = ["main"]
 
 app = Flask(__name__)
 
-# Set .env variables
 app.static_folder = STATIC_URL
 app.debug = DEBUG
 
-# Register apps
 app.register_blueprint(home_bp, name="home")
 app.register_blueprint(device_bp, name="devices")
 
 
 def main() -> None:
-    # preprocessing_all_files()
-    # make_graph("AE33-S09-01249")
-    # make_graph("TCA08")
-    # make_graph("Web_MEM")
-    # make_graph("LVS")
-    # make_graph("PNS")
+    preprocessing_all_files()
+    make_graph("AE33-S09-01249")
+    make_graph("TCA08")
+    make_graph("Web_MEM")
+    make_graph("LVS")
+    make_graph("PNS")
     db_session.global_init("devices/db/blogs.db")
     app.run()
 
