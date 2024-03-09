2d97d61fe5d77b2c98f606e1a8b78b66475cc8ef
Ivan Kaziev <ivkaziev@yandex.ru>
2024-03-09 12:44:21 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-from views.homepage.home import home_bp
+from config import STATIC_URL, DEBUG
 from flask import Flask
-from config import STATIC_URL
-from graph_funcs import preprocessing_all_files, make_graph
+from graph_funcs import make_graph, preprocessing_all_files
+
+from homepage.home import home_bp
 
 app = Flask(__name__)
 app.static_folder = STATIC_URL
+app.debug = DEBUG
 app.register_blueprint(home_bp)
 
 
 if __name__ == "__main__":
     preprocessing_all_files()
-    make_graph('AE33-S09-01249')
-    make_graph('TCA08')
-    make_graph('Web_MEM')
-    make_graph('LVS')
-    make_graph('PNS')
-    app.run(debug=True)
+    make_graph("AE33-S09-01249")
+    make_graph("TCA08")
+    make_graph("Web_MEM")
+    make_graph("LVS")
+    make_graph("PNS")
+    app.run()
