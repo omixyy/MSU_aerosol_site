93f0a7ef254703b292d0025dd319395800445db1
Ivan Kaziev <ivkaziev@yandex.ru>
2024-03-16 23:03:34 +0300

    Add typization

--- msu_aerosol/device/views.py
+++ msu_aerosol/device/views.py
@@ -2,15 +2,15 @@ from datetime import datetime
 
 from flask import Blueprint, render_template
 
-__all__ = ["device"]
+__all__ = []
 
-device_bp = Blueprint("device", __name__, url_prefix="/")
+device_bp: Blueprint = Blueprint("device", __name__, url_prefix="/")
 
 
 @device_bp.route("/device")
 def device() -> str:
     return render_template(
         "device/device.html",
-        now=datetime.utcnow(),
+        now=datetime.now(),
         view_name="device",
     )
