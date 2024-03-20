08cdca8510fc3fd1bb67a9185e1d6511efba9d52
Ivan Kaziev <ivkaziev@yandex.ru>
2024-03-20 20:18:50 +0300

    Add devices list

--- msu_aerosol/homepage/views.py
+++ msu_aerosol/homepage/views.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from flask import Blueprint, render_template
 
+from msu_aerosol import models
+
 __all__ = ["index"]
 
 home_bp: Blueprint = Blueprint("home", __name__, url_prefix="/")
@@ -9,8 +11,10 @@ home_bp: Blueprint = Blueprint("home", __name__, url_prefix="/")
 
 @home_bp.route("/")
 def index() -> str:
+    all_devices: list[models.Device] = models.Device.query.all()
     return render_template(
         "homepage/home.html",
         now=datetime.now(),
         view_name="homepage",
+        devices=all_devices,
     )
