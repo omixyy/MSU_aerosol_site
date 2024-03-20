6e25b37e38fe7bf049996f8164bea0673b6a48b4
Ivan Kaziev <ivkaziev@yandex.ru>
2024-03-20 20:19:14 +0300

    Add device description

--- msu_aerosol/msu_aerosol/admin.py
+++ msu_aerosol/msu_aerosol/admin.py
@@ -1,14 +1,26 @@
 from flask import Flask
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from msu_aerosol.models import Complex, db, Device
+from msu_aerosol.models import (
+    Complex,
+    db,
+    Device,
+    TextFieldView,
+)
+from flask_admin import AdminIndexView
 
 __all__ = ["init_admin"]
 
-admin: Admin = Admin()
+admin: Admin = Admin(
+    index_view=AdminIndexView(
+        name="Home",
+        template="admin/index.html",
+        url="/admin",
+    ),
+)
 
 
 def init_admin(app: Flask):
     admin.init_app(app)
     admin.add_view(ModelView(Complex, db.session))
-    admin.add_view(ModelView(Device, db.session))
+    admin.add_view(TextFieldView(Device, db.session))
