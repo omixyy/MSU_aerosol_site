79eeb3c45755345a63fb4e9c954dbb276323480f
Ivan Kaziev <ivkaziev@yandex.ru>
2024-03-21 09:25:35 +0300

    Add device endpoint

--- msu_aerosol/run.py
+++ msu_aerosol/run.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
-from homepage.views import home_bp
 from device.views import device_bp
+from homepage.views import home_bp
 from msu_aerosol import config
 from msu_aerosol.admin import init_admin
 from msu_aerosol.graph_funcs import make_graph, preprocessing_all_files
@@ -20,12 +20,12 @@ with app.app_context():
 
 
 def main() -> None:
-    # preprocessing_all_files()
-    # make_graph("AE33-S09-01249")
-    # make_graph("TCA08")
-    # make_graph("Web_MEM")
-    # make_graph("LVS")
-    # make_graph("PNS")
+    preprocessing_all_files()
+    make_graph("AE33-S09-01249")
+    make_graph("TCA08")
+    make_graph("Web_MEM")
+    make_graph("LVS")
+    make_graph("PNS")
     app.run()
 
 
