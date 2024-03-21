1dabec26538f68ea230a1cd82fd4c35adb3c2d18
Ivan Kaziev <ivkaziev@yandex.ru>
2024-03-21 09:25:49 +0300

    Fix __all__

--- msu_aerosol/homepage/views.py
+++ msu_aerosol/homepage/views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template
 
 from msu_aerosol import models
 
-__all__ = ["index"]
+__all__: list = []
 
 home_bp: Blueprint = Blueprint("home", __name__, url_prefix="/")
 
