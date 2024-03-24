29c51cc4a5edd2395b1f744393f0073cb7882f3c
Ivan Kaziev <ivkaziev@yandex.ru>
2024-03-24 15:35:30 +0300

    Add admin panel homepage

--- msu_aerosol/msu_aerosol/admin.py
+++ msu_aerosol/msu_aerosol/admin.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from flask_admin import Admin
-from flask_admin import AdminIndexView
+from flask_admin import AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
+
+import json
 
 from msu_aerosol.models import (
     Complex,
@@ -10,10 +12,52 @@ from msu_aerosol.models import (
     TextFieldView,
 )
 
-__all__ = ["init_admin"]
+__all__ = []
+
+
+class AdminHomeView(AdminIndexView):
+    def __init__(
+            self, 
+            name=None, 
+            category=None, 
+            endpoint=None, 
+            url=None, 
+            template='admin/index.html', 
+            menu_class_name=None, 
+            menu_icon_type=None, 
+            menu_icon_value=None,
+        ) -> None:
+        super().__init__(
+            name, 
+            category, 
+            endpoint, 
+            url, 
+            template, 
+            menu_class_name, 
+            menu_icon_type, 
+            menu_icon_value,
+        )
+
+    @expose("/", methods=["GET", "POST"])
+    def create_settings_form(self) -> str:
+        if request.method == "GET":
+            all_devices = Device.query.all()
+            with open("msu_aerosol/config_devices.json", "r") as jsonf:
+                data = json.load(jsonf)
+                cols = {dev: data[dev]["cols"] for dev in data.keys()}
+                print(cols)
+                time_cols = {dev: data[dev]["time_cols"] for dev in data.keys()}
+            return self.render(
+                "admin/admin_home.html",
+                devices=all_devices,
+                time_cols=time_cols,
+                cols=cols,
+            )
+
 
 admin: Admin = Admin(
-    index_view=AdminIndexView(
+    template_mode="bootstrap4",
+    index_view=AdminHomeView(
         name="Home",
         template="admin/index.html",
         url="/admin",
