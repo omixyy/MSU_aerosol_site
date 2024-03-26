26c8892cee9e055dcf8aab23aa374b3d87b316e9
Ivan Kaziev <ivkaziev@yandex.ru>
2024-03-26 11:46:58 +0300

    Add login handlers

--- msu_aerosol/users/forms.py
+++ msu_aerosol/users/forms.py
@@ -1,5 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, StringField, SubmitField
+from wtforms import (
+    BooleanField,
+    EmailField,
+    PasswordField,
+    StringField,
+    SubmitField,
+)
 from wtforms.validators import DataRequired
 
 __all__: list = []
@@ -18,8 +24,21 @@ class RegisterForm(FlaskForm):
         "Повторите пароль",
         validators=[DataRequired()],
     )
-    name = StringField(
+    username = StringField(
         "Имя пользователя",
         validators=[DataRequired()],
     )
     submit = SubmitField("Отправить")
+
+
+class LoginForm(FlaskForm):
+    username = StringField(
+        "Логин",
+        validators=[DataRequired()],
+    )
+    password = PasswordField(
+        "Пароль",
+        validators=[DataRequired()],
+    )
+    remember_me = BooleanField("Запомнить меня")
+    submit = SubmitField("Войти")
--- msu_aerosol/users/views.py
+++ msu_aerosol/users/views.py
@@ -1,49 +1,91 @@
 from datetime import datetime
 
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, redirect, render_template, url_for, request, session
 from flask_login import current_user
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from msu_aerosol.admin import get_complexes_dict
-from users.forms import RegisterForm
+from msu_aerosol.models import User, db
+from users.forms import RegisterForm, LoginForm
 
 __all__: list = []
 
 
-def set_password(self, password):
+def set_password(self, password: str) -> None:
     self.hashed_password = generate_password_hash(password)
 
 
-def check_password(self, password):
+def check_password(self, password: str) -> bool:
     return check_password_hash(self.hashed_password, password)
 
 
 register_bp: Blueprint = Blueprint("register", __name__, url_prefix="/")
+login_bp: Blueprint = Blueprint("login", __name__, url_prefix="/")
+
+
+@login_bp.route("/login", methods=["GET", "POST"])
+def login() -> str:
+    complex_to_device = get_complexes_dict()
+    form = LoginForm()
+    if request.method == "GET":
+        return render_template(
+            "users/login.html",
+            title="Войти",
+            form=form,
+            complex_to_device=complex_to_device,
+            now=datetime.now(),
+            view_name="login",
+            user=current_user,
+        )
+
 
 
 @register_bp.route("/register", methods=["GET", "POST"])
-def register():
-    form = RegisterForm()
+def register() -> str:
     complex_to_device = get_complexes_dict()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
+    form = RegisterForm()
+    if request.method == "GET":
+        return render_template(
+            "users/register.html",
+            title="Регистрация",
+            form=form,
+            complex_to_device=complex_to_device,
+            now=datetime.now(),
+            view_name="registration",
+            user=current_user,
+        )
+
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password_again = request.form.get("password_again")
+        email = request.form.get("email")
+        if password != password_again:
             return render_template(
                 "users/register.html",
-                title="Регистрация",
                 form=form,
-                message="Пароли не совпадают",
+                error="Пароли не совпадают",
                 complex_to_device=complex_to_device,
                 now=datetime.now(),
                 view_name="registration",
             )
-        return redirect("/login")
-
-    return render_template(
-        "users/register.html",
-        title="Регистрация",
-        form=form,
-        complex_to_device=complex_to_device,
-        now=datetime.now(),
-        view_name="registration",
-        user=current_user,
-    )
+        
+        if User.query.filter_by(username=username).first():
+            return render_template(
+                "users/register.html",
+                form=form,
+                message="Пользователь с таким именем уже существует",
+                complex_to_device=complex_to_device,
+                now=datetime.now(),
+                view_name="registration",
+            )
+        
+        new_user = User(
+            username=username,
+            hashed_password=generate_password_hash(password),
+            email=email,
+        )
+    
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("home.index"))
