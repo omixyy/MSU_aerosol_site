23032fd2af858bf5e161bf096c75b250ea9f183b
Ivan Kaziev <ivkaziev@yandex.ru>
2024-04-04 21:44:55 +0300

    Create new tests

--- msu_aerosol/tests/test_forms.py
+++ msu_aerosol/tests/test_forms.py
@@ -1,11 +1,13 @@
 import unittest
 
-from flask import request, url_for
+from flask import url_for
 from flask_login import current_user
 import parameterized
 
 from main import app
-from msu_aerosol.models import db, User
+from msu_aerosol.models import db
+
+__all__: list = []
 
 
 class TestRegisterForm(unittest.TestCase):
@@ -16,7 +18,7 @@ class TestRegisterForm(unittest.TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
-    
+
     @parameterized.parameterized.expand(
         [
             (
@@ -43,7 +45,7 @@ class TestRegisterForm(unittest.TestCase):
                     "password_again": "GEGW#Q@$GW",
                 },
             ),
-        ]
+        ],
     )
     def test_register_form_authenticated(self, data):
         with self.app.app_context(), self.app.test_request_context():
@@ -54,7 +56,7 @@ class TestRegisterForm(unittest.TestCase):
             )
 
             self.assertTrue(current_user.is_authenticated)
-    
+
     @parameterized.parameterized.expand(
         [
             (
@@ -81,7 +83,7 @@ class TestRegisterForm(unittest.TestCase):
                     "password_again": "GEGW#ase2w2@$GW",
                 },
             ),
-        ]
+        ],
     )
     def test_register_form_not_authenticated(self, data):
         with self.app.app_context(), self.app.test_request_context():
@@ -102,17 +104,17 @@ class TestLoginForm(unittest.TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
-    
+
     def test_auth(self):
         with self.app.app_context(), self.app.test_request_context():
             self.client.post(
-            url_for("registration.register"),
-            data={
-                "login": "TestLoginForm",
-                "email": "testlogin@email.ru",
-                "password": "_)@*(HF3-08fh)",
-                "password_again": "_)@*(HF3-08fh)",
-            },
-            follow_redirects=True,
-        )
+                url_for("registration.register"),
+                data={
+                    "login": "TestLoginForm",
+                    "email": "testlogin@email.ru",
+                    "password": "_)@*(HF3-08fh)",
+                    "password_again": "_)@*(HF3-08fh)",
+                },
+                follow_redirects=True,
+            )
             self.assertTrue(current_user.is_authenticated)
