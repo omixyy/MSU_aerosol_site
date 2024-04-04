9493a8e8e12b894b52dea3dabe9df2a8d5bc87ce
Ivan Kaziev <ivkaziev@yandex.ru>
2024-04-04 21:43:07 +0300

    Create new tests

---
+++ msu_aerosol/tests/test_forms.py
@@ -0,0 +1,118 @@
+import unittest
+
+from flask import request, url_for
+from flask_login import current_user
+import parameterized
+
+from main import app
+from msu_aerosol.models import db, User
+
+
+class TestRegisterForm(unittest.TestCase):
+    def setUp(self) -> None:
+        self.app = app
+        self.app.config["TESTING"] = True
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+    
+    @parameterized.parameterized.expand(
+        [
+            (
+                {
+                    "login": "TestLogin1",
+                    "email": "test1@email.ru",
+                    "password": "(@Q*DYqdw12)e23",
+                    "password_again": "(@Q*DYqdw12)e23",
+                },
+            ),
+            (
+                {
+                    "login": "TestLogin2",
+                    "email": "test2@email.ru",
+                    "password": "asddsgfew32",
+                    "password_again": "asddsgfew32",
+                },
+            ),
+            (
+                {
+                    "login": "TestLogin3",
+                    "email": "test3@email.ru",
+                    "password": "GEGW#Q@$GW",
+                    "password_again": "GEGW#Q@$GW",
+                },
+            ),
+        ]
+    )
+    def test_register_form_authenticated(self, data):
+        with self.app.app_context(), self.app.test_request_context():
+            self.client.post(
+                url_for("registration.register"),
+                data=data,
+                follow_redirects=True,
+            )
+
+            self.assertTrue(current_user.is_authenticated)
+    
+    @parameterized.parameterized.expand(
+        [
+            (
+                {
+                    "login": "TestLogin1",
+                    "email": "testl1@email.ru",
+                    "password": "(@Q*DYqdq3werw12)e23",
+                    "password_again": "(@Q*DYqdw12)e23",
+                },
+            ),
+            (
+                {
+                    "login": "TestLogin2",
+                    "email": "testl2@email.ru",
+                    "password": "asddsgfew32",
+                    "password_again": "asddsasfg4ggfew32",
+                },
+            ),
+            (
+                {
+                    "login": "TestLogin3",
+                    "email": "testl3@email.ru",
+                    "password": "GEGW#Q@$GW",
+                    "password_again": "GEGW#ase2w2@$GW",
+                },
+            ),
+        ]
+    )
+    def test_register_form_not_authenticated(self, data):
+        with self.app.app_context(), self.app.test_request_context():
+            self.client.post(
+                url_for("registration.register"),
+                data=data,
+                follow_redirects=True,
+            )
+
+            self.assertFalse(current_user.is_authenticated)
+
+
+class TestLoginForm(unittest.TestCase):
+    def setUp(self):
+        self.app = app
+        self.app.config["TESTING"] = True
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+    
+    def test_auth(self):
+        with self.app.app_context(), self.app.test_request_context():
+            self.client.post(
+            url_for("registration.register"),
+            data={
+                "login": "TestLoginForm",
+                "email": "testlogin@email.ru",
+                "password": "_)@*(HF3-08fh)",
+                "password_again": "_)@*(HF3-08fh)",
+            },
+            follow_redirects=True,
+        )
+            self.assertTrue(current_user.is_authenticated)
--- msu_aerosol/tests/test_urls.py
+++ msu_aerosol/tests/test_urls.py
@@ -4,11 +4,6 @@ import unittest
 from flask import request, url_for
 
 from main import app
-from msu_aerosol.models import (
-    Complex,
-    db,
-    Device,
-)
 
 __all__: list = []
 
@@ -20,25 +15,6 @@ class TestStaticURL(unittest.TestCase):
         self.client = self.app.test_client()
         self.app_context = self.app.app_context()
         self.app_context.push()
-        db.create_all()
-
-        complex = Complex(
-            name="TestComplex",
-        )
-        db.session.add(complex)
-        db.session.commit()
-        device = Device(
-            name="TestDevice",
-            link="https://disk.yandex.ru/d/3iruCwAKaGUD7g",
-            complex_id=Complex.query.first().id,
-        )
-        db.session.add(device)
-        db.session.commit()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        self.app_context.pop()
 
     def test_url(self):
         with self.app.app_context(), self.app.test_request_context():
