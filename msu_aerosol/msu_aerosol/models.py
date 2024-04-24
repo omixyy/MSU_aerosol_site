0db819fa6292c6d9237acda1b9435748ff109ddf
Ivan Kaziev <ivkaziev@yandex.ru>
2024-04-24 21:15:31 +0300

    Make name a non-nullable field

--- msu_aerosol/msu_aerosol/models.py
+++ msu_aerosol/msu_aerosol/models.py
@@ -17,7 +17,7 @@ db: SQLAlchemy = SQLAlchemy()
 class BaseModel(db.Model):
     __abstract__ = True
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, unique=True)
+    name = db.Column(db.String, unique=True, nullable=False)
 
 
 class BaseColumnModel(db.Model):
