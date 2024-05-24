87614dc508166dfb70de18d3f9b5f16930df4749
omixyy <ivan.kaziev07@gmail.com>
2024-05-24 11:54:39 +0300

    Fix choose_range()

--- msu_aerosol/msu_aerosol/graph_funcs.py
+++ msu_aerosol/msu_aerosol/graph_funcs.py
@@ -161,8 +161,9 @@ def choose_range(device: str, app=None) -> tuple[pd.Timestamp, pd.Timestamp]:
         if i.use
     ][0]
     list_files = os.listdir(f'proc_data/{device}')
+    proc_data = f'proc_data/{device}/{max(list_files)}'
     max_date = pd.to_datetime(
-        pd.read_csv(f'proc_data/{device}/{max(list_files)}')[time_col].iloc[-1],
+        pd.read_csv(proc_data)[time_col].iloc[-1],
     )
     min_date = max_date - timedelta(days=14)
     return min_date, max_date
