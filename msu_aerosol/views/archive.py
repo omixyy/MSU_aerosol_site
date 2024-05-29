2ae9feecc01cc9eb1fce85a19292492d418e5ad7
omixyy <ivan.kaziev07@gmail.com>
2024-05-29 13:24:48 +0300

    Archive is ready

--- msu_aerosol/views/archive.py
+++ msu_aerosol/views/archive.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 
 from flask import Blueprint, render_template
 from flask_login import current_user
@@ -11,7 +12,7 @@ __all__: list = []
 archive_bp: Blueprint = Blueprint('about', __name__, url_prefix='/')
 
 
-@archive_bp.route('/archive', methods=['GET', 'POST'])
+@archive_bp.route('/archive', methods=['GET'])
 def archive() -> str:
     complex_to_device: dict[Complex, list[Device]] = get_complexes_dict()
     return render_template(
@@ -20,4 +21,20 @@ def archive() -> str:
         view_name='archive',
         complex_to_device=complex_to_device,
         user=current_user,
+    )
+
+
+@archive_bp.route('/archive/<int:device_id>', methods=['GET'])
+def get_device_from_archive(device_id: int) -> str:
+    device: Device = Device.query.get_or_404(device_id)
+    complex_to_device: dict[Complex, list[Device]] = get_complexes_dict()
+    files = os.listdir(f'data/{device.full_name}')
+    return render_template(
+        'archive/device_archive.html',
+        now=datetime.now(),
+        view_name='device_archive',
+        device=device,
+        user=current_user,
+        complex_to_device=complex_to_device,
+        files=files,
     )
