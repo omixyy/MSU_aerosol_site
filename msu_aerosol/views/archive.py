7a888788ab49ec7a1e5d55b887e60ff2698ba192
omixyy <ivan.kaziev07@gmail.com>
2024-06-12 16:54:06 +0300

    Fix files display in archive

--- msu_aerosol/views/archive.py
+++ msu_aerosol/views/archive.py
@@ -59,7 +59,7 @@ class DeviceArchive(MethodView):
             device.full_name = disk.get_public_meta(device.link)['name']
             db.session.commit()
         path = f'data/{device.full_name}'
-        files = sorted(os.listdir(path), key=lambda x: x[0:6])
+        files = sorted(os.listdir(path), key=lambda x: datetime.strptime(x, '%Y-%m'))
         return render_template(
             'archive/device_archive.html',
             now=datetime.now(),
