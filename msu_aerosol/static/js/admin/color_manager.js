a94cd785d4569541ebe8b2d85b4ffaf883fd1c9f
omixyy <ivan.kaziev07@gmail.com>
2024-07-03 13:43:03 +0300

    Delete extra script

--- msu_aerosol/static/js/admin/color_manager.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('#colorPicker').on('input', function() {
-    var color = $(this).val();
-    $('#selectedColorDisplay').css('background-color', color);
-    $('#selectedColorHex').text('HEX: ' + color);
-  });
-});
